f6ee1ba941721e313a603f6c2729360f016276ad
Kyo young Lim <limkyouyou@gmail.com>
2023-07-13 13:56:27 -0400

    Add get user hand function

--- rock_paper_scissors/rock_paper_scissors.py
+++ rock_paper_scissors/rock_paper_scissors.py
@@ -16,4 +16,16 @@ class RockPaperScissors():
     def get_comp_hand(self):
         hand = randint(-1, 1)
         return hand
+
+    def get_user_hand(self):
+        while True:
+            hand = input('Choose Rock (R), Paper (P), or Scissors (S): ').lower()
+            if hand == 'r':
+                return self.hands['rock']
+            elif hand == 'p':
+                return self.hands['paper']
+            elif hand == 's':
+                return self.hands['scissors']
+            else:
+                print('Invalid input. Please type in R for Rock, P for Paper, or S for Scissors.')
         
